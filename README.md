fe09e99aaf9d46c3b3180f97bf53a05d0e148afb
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-11-08 00:47:08 +0100

    init forwarder

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-# Metrics Server
+# Metrics Forwarder
 
-[![Go Report Card](https://goreportcard.com/badge/github.com/Drakkar-Software/Metrics-Server)](https://goreportcard.com/report/github.com/Drakkar-Software/Metrics-Server)
-
-Metrics server for [OctoBot](https://github.com/Drakkar-Software/OctoBot) project. Collects anonymous usage data in order to improve [OctoBot](https://github.com/Drakkar-Software/OctoBot) user experience and evolve towards what users are the most interested in.
-
-This server is use to by the /community tab of OctoBot.
+Metrics forwarder for [OctoBot](https://github.com/Drakkar-Software/OctoBot) project. Redirects metrics requests towards the remote metrics server.
 
 # Installation
 
---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/url"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	//controller.Init()
+	e := echo.New()
+	e.Use(middleware.Logger())
+	// Setup proxy
+	url1, err := url.Parse(getURL())
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: url1,
+		},
+	}
+	e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
+
+	e.Logger.Fatal(e.Start(":" + getPort()))
+}
+
+func getURL() string {
+	metricsServerURL := os.Getenv("METRICS_SERVER_URL")
+
+	if metricsServerURL == "" {
+		panic("Metrics server url not found in environment variables. Please set METRICS_SERVER_URL env variable.")
+	}
+	if string(metricsServerURL[len(metricsServerURL)-1]) == "/" {
+		metricsServerURL = metricsServerURL[:len(metricsServerURL)-1]
+	}
+
+	return metricsServerURL
+}
+
+func getPort() string {
+	port, exists := os.LookupEnv("PORT")
+	if !exists || port == "" {
+		port = "8080"
+	}
+	return port
+}
+
